f64aa60b66728596f0f830e8adbfdc3afeb6f721
root <root@kwahvmcjxy.local>
2024-11-06 16:34:03 +0000

    Save local changes

--- sayt/settings.py
+++ sayt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)9-1&-nw!r49#660ayo^xw6#-f-k63xf_#%$20&7a2*fp)vd0t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
